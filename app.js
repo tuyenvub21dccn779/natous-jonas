16550a6210a994a940bae9ab81027d77db84b860
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-06-03 02:35:34 +0700

    56 - Handling DELETE Requests

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+app.patch('api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -84,6 +84,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     data: {
       tour: '<Updated tour here...>',
     },
+  });
+});
+
+app.delete('api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: null,
   });
 });
 
