93be16d0b35e0347b798fe5b801bcea111dcdc35
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-06-05 14:03:01 +0700

    98 - Making the API Better Limiting Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,6 +34,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
