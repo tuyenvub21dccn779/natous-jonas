11a00cb0672ba36f04dee3b7474f7e94cccb4ad0
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-06-06 12:14:16 +0700

    112 - Handling Unhandled Routes

--- app.js
+++ app.js
@@ -24,8 +24,14 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTE
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
